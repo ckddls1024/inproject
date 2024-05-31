ab83c1f0e4f48abeb4d3bf28c89fb3ce7ea7cf16
qhal5 <alfkk122@naver.com>
2024-05-31 10:19:11 +0900

    test

---
+++ src/main/java/com/example/InProject/InProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.InProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/InProject/InProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
